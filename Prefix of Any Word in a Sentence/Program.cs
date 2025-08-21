124f77fe36269cdf0b6674193481b593a451ef65
Hari Shankar Singh <hari@lmg8.tech>
2025-08-21 07:58:44 +0530

    Add project files.

---
+++ Prefix of Any Word in a Sentence/Program.cs
@@ -0,0 +1,21 @@
+﻿class program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(IndexOfPerfix("i burger eating burger", "burg")); //output=4
+        Console.WriteLine(IndexOfPerfix("this problem is an easy problem","pro")); //output=2
+        Console.WriteLine(IndexOfPerfix("i am tired", "you")); //output=-1
+    }
+    public static int IndexOfPerfix(string s, string t)
+    {
+        string[] words = s.Split(' ');
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (words[i].StartsWith(t))
+            {
+                return i + 1;
+            }
+        }
+        return -1;
+    }
+}
